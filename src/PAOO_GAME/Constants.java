4b528e6810c038f43f46f3bd7c6a893387ebea94
GCipry3 <ciprian.galbeaza@student.tuiasi.ro>
2022-05-13 18:36:46 +0300

    PAOO Game

---
+++ src/PAOO_GAME/Constants.java
@@ -0,0 +1,14 @@
+package PAOO_GAME;
+
+public final class Constants {
+    private Constants(){}
+    public final static int shurikenCirclePixels =128;
+    public final static int tileWidth = 32;
+    public final static int tileHeight = 32;
+    public final static int playerSpeed =3;
+    public static final int playerWidth =32;
+    public final static int playerHeight =32;
+    public final static int playerDrawScale = 2;
+    public final static int []wallCollisions={1,5};
+    public final static int []nextLevelCollision={2};
+}
---
+++ src/PAOO_GAME/Enemy/Ogre.java
@@ -0,0 +1,20 @@
+package PAOO_GAME.Enemy;
+
+import PAOO_GAME.Player.Player;
+
+public class Ogre extends Enemy{
+    @Override
+    public void move(int x1, int x2, Player player) {
+
+    }
+
+    @Override
+    public void atack(Player player) {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
